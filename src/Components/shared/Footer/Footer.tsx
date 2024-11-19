42c11e300e67542a0b70122ef7d56c8e9b1255b2
Danniel Navas <dannielnavas@gmail.com>
2024-11-18 19:13:39 -0500

    Actualizar el pie de página para usar un diseño fijo y modificar los colores de fondo y primer plano en el CSS

--- src/Components/shared/Footer/Footer.tsx
+++ src/Components/shared/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white text-center p-4 sticky bottom-0">
+    <footer className="bg-gray-800 text-white text-center flex items-center justify-center p-4 fixed bottom-0 w-full h-24">
       <p>&copy; 2024 Tripix</p>
     </footer>
   );
